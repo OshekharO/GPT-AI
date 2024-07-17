9cad0d58a4ab371c00ff8de04e0da1e23213ce4b
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-07-17 22:09:43 +0530

    Feat: cheatgpt

--- index.js
+++ index.js
@@ -233,6 +233,36 @@ app.post('/chat/v5', async (req, res) => {
   }
 });
 
+// API Route v6 - cheatgpt.app
+app.post('/chat/v6', async (req, res) => {
+  const { userMessage } = req.body;
+
+  const apiUrl = 'https://cheatgpt.app/message';
+  const headers = {
+            'Content-Type': 'application/json',
+            'accept': '*/*',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36',
+            'baggage': 'sentry-environment=production,sentry-release=df10cf955a0c4f0afba4aa0b43d0b66aa90f0c8d,sentry-public_key=df1a32ff871ba9c6a799586a64a2b2bd,sentry-trace_id=9f843dd5b2d34785902862db7602dbf6',
+            'sentry-trace': '9f843dd5b2d34785902862db7602dbf6-adbbfa3670133711',
+            'cookie': '_gcl_au=1.1.162648429.1720286077; _ga=GA1.1.184148824.1720286077; sb-vfkzchgbokxuyihsqfpq-auth-token=%5B%22eyJhbGciOiJIUzI1NiIsImtpZCI6IkNKb0dJc3RhKzhlWFVZQmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3Zma3pjaGdib2t4dXlpaHNxZnBxLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiIzMWM1ODdjYy0yMzJlLTRlMTUtODM1Mi1jYjcxNDMxMmE2MTIiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzIxMjM2MjI0LCJpYXQiOjE3MjEyMzI2MjQsImVtYWlsIjoib21lZXBkMDA5QGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZ29vZ2xlIiwicHJvdmlkZXJzIjpbImdvb2dsZSJdfSwidXNlcl9tZXRhZGF0YSI6eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSVpUYkJaN2lwSzhpYW5uS0FIY196b3M5V1g5TW1VVU10b1h4b1djQm15d3hTbnVxM249czk2LWMiLCJlbWFpbCI6Im9tZWVwZDAwOUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZnVsbF9uYW1lIjoiU2Frc2hhbSBTaGVraGVyIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwibmFtZSI6IlNha3NoYW0gU2hla2hlciIsInBob25lX3ZlcmlmaWVkIjpmYWxzZSwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0laVGJCWjdpcEs4aWFubktBSGNfem9zOVdYOU1tVVVNdG9YeG9XY0JteXd4U251cTNuPXM5Ni1jIiwicHJvdmlkZXJfaWQiOiIxMTgyOTc5OTg3MTkxMjAyMDM3OTYiLCJzdWIiOiIxMTgyOTc5OTg3MTkxMjAyMDM3OTYifSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJvYXV0aCIsInRpbWVzdGFtcCI6MTcyMDI4NjA4OH1dLCJzZXNzaW9uX2lkIjoiMjNhNzRmYTUtYzFiYy00YTQ1LTg4NjctZGVmMzJiNTNlYjg3IiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.rljeUaNl6D2nuHPPtAnwY-u2eGkC7_eYSmOuBfA5V7o%22%2C%22pVUrtvLIFZ9erQxG1j6hDg%22%2Cnull%2Cnull%2Cnull%5D; _ga_229BFDWDCL=GS1.1.1721232624.2.1.1721232666.0.0.0'
+        };
+  const body = {
+  "context": "You're a concise answerer. Respond to the user's message concisely",
+  "model": "gpt-3.5-turbo",
+  "temperature": 0.2,
+  "responseType": "ConciseAnswer",
+  "message": userMessage
+}
+
+  try {
+    const response = await axios.post(apiUrl, body, { headers });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Something went wrong with API v6 Cookies' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
